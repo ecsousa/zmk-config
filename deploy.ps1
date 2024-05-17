a45141e231b1a391ae0c6bc86cbfa0deea8e646a
Eduardo Sousa <ecsousa@gmail.com>
2024-05-17 10:43:22 -0700

    Merge accent layers

--- deploy.ps1
+++ deploy.ps1
@@ -1,11 +1,15 @@
 Write-Warning "Deploying Glove80 firmware..."
 
+Write-Warning "Waiting for left hand to be connected..."
+
 while( (Get-Volume | ? { $_.FileSystemLabel -eq 'GLV80RHBOOT' }) -eq $null )
 {
     Start-Sleep -Seconds 0.3
 }
 
 cp -Verbose .\glove80.uf2 (Get-Volume | ? { $_.FileSystemLabel -eq 'GLV80RHBOOT' }).path
+
+Write-Warning "Waiting for right hand to be connected..."
 
 while( (Get-Volume | ? { $_.FileSystemLabel -eq 'GLV80LHBOOT' }) -eq $null )
 {
